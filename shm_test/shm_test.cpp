66fcd927f99f5731bb78834b5b7c40e567fc71ea
Hujun <hujun1413@foxmail.com>
2017-08-07 10:03:52 +0800

    Add multiple procedure to rostest
    to evaluate the efficiency of shm_transport, test by using multiple procedure

--- shm_test/shm_test.cpp
+++ shm_test/shm_test.cpp
@@ -7,77 +7,165 @@
 #define MSGLEN (1024 * 1024 * 3)
 #define HZ (1)
 
-std_msgs::String::ConstPtr str_received;
-std_msgs::UInt64::ConstPtr uint64_received;
+std::string str_send;
+uint64_t uint64_send;
 
+static int count = 0;
 void strCallback(const std_msgs::String::ConstPtr & msg)
 {
-    str_received = msg;
+    std::stringstream ss;
+    ss << "message #" << count << "...";
+    for (int i = 0; i < MSGLEN - 20; i++)
+    ss << " ";
+
+    EXPECT_STREQ(ss.str().c_str(), msg->data.c_str());
+    ++count;
 }
 
 void uint64Callback(const std_msgs::UInt64::ConstPtr & msg)
 {
-    uint64_received = msg;
+    EXPECT_EQ(count, msg->data);
+    ++count;
 }
 
 
-TEST(ShmTester, stringTest)
+/*TEST(ShmTester, stringTest)
 {
-    ros::NodeHandle n;
-    shm_transport::Topic t(n);
-    shm_transport::Subscriber< std_msgs::String > s = t.subscribe("shm_test_topic", 60, strCallback);
-    shm_transport::Publisher p = t.advertise< std_msgs::String >("shm_test_topic", HZ, HZ * MSGLEN);
-
-    ros::Rate loop_rate(HZ);
-    int count = 0;
-    while (ros::ok() && count <= 5)
+    pid_t child;
+    if((child = fork()) == -1)
     {
-        std::stringstream ss;
-        ss << "message #" << count << "...";
-        for (int i = 0; i < MSGLEN - 20; i++)
-          ss << " ";
-        std_msgs::String msg;
-        msg.data = ss.str();
-
-        p.publish(msg);
-
-        loop_rate.sleep();
-        ros::spinOnce();
-
-        count++;
-        EXPECT_STREQ(msg.data.c_str(), str_received->data.c_str());
+       perror("fork error\n");
+       exit(EXIT_FAILURE);
     }
-}
+    else if(child == 0)     //in child process
+    {
+        printf("child1 %d\n", getpid());
+        int argc = 1;
+        char * argv[] = {(char *)"shm_talker", NULL};
+        ros::init(argc, (char **)argv, "shm_talker", ros::init_options::AnonymousName);
+        ros::NodeHandle n;
+        shm_transport::Topic t(n);
+        shm_transport::Publisher p = t.advertise< std_msgs::String >("shm_test_topic", HZ, (HZ+1) * MSGLEN);
+        sleep(1);
+        ros::Rate loop_rate(HZ);
+        int count = 0;
+        while (ros::ok() && count <= 5)
+        {
+            std::stringstream ss;
+            ss << "message #" << count << "...";
+            for (int i = 0; i < MSGLEN - 20; i++)
+            ss << " ";
+            std_msgs::String msg;
+            msg.data = ss.str();
+
+            str_send = msg.data;
+            p.publish(msg);
+
+            ros::spinOnce();
+            loop_rate.sleep();
+
+            count++;
+        }
+        ros::shutdown();
+        printf("-222222222222222222222222222hahahha\n");
+    }
+    else
+    {
+        printf("father1 %d\n", getpid());
+        int argc = 1;
+        char * argv[] = {(char *)"shm_listener", NULL};
+        ros::init(argc, (char **)argv, "shm_listener", ros::init_options::AnonymousName);
+        ros::NodeHandle n;
+        shm_transport::Topic t(n);
+        shm_transport::Subscriber< std_msgs::String > s = t.subscribe("shm_test_topic", 60, strCallback);
+        ros::Rate loop_rate(60);
+        while(ros::ok && count <= 5)
+        {
+            //ROS_INFO("%d",count);
+            loop_rate.sleep();
+            ros::spinOnce();
+        }
+        ros::shutdown();
+
+        wait(NULL);
+        printf("------------------------hahahha\n");
+    }
+}*/
 
 TEST(ShmTester, uint64Test)
 {
-    ros::NodeHandle n;
-    shm_transport::Topic t(n);
-    shm_transport::Subscriber< std_msgs::UInt64 > s = t.subscribe("shm_test_topic", 60, uint64Callback);
-
-    shm_transport::Publisher p = t.advertise< std_msgs::UInt64 >("shm_test_topic", HZ, HZ * 1000);
-
-    ros::Rate loop_rate(HZ);
-    int count = 0;
-    while (ros::ok() && count <= 5)
+    pid_t child;
+    if((child = fork()) == -1)
     {
-        std_msgs::UInt64 msg;
-        msg.data = count;
+        perror("fork error\n");
+        exit(EXIT_FAILURE);
+    }
+    else if(child == 0)     //in child process
+    {
+        printf("child2 %d\n", getpid());
+        int argc = 1;
+        char * argv[] = {(char *)"shm_talker", NULL};
+        ros::init(argc, (char **)argv, "shm_talker", ros::init_options::AnonymousName);
+        ros::NodeHandle n;
+        shm_transport::Topic t(n);
+        shm_transport::Publisher p = t.advertise< std_msgs::UInt64 >("/shm_test_topic1", HZ, HZ * 1000);
+        sleep(1);
+        ros::Rate loop_rate(HZ);
+        int count = 0;
+        while (ros::ok() && count <= 5)
+        {
+            std_msgs::UInt64 msg;
+            msg.data = count;
 
-        p.publish(msg);
+            uint64_send = msg.data;
+            p.publish(msg);
 
-        loop_rate.sleep();
-        ros::spinOnce();
+            loop_rate.sleep();
+            ros::spinOnce();
 
-        count++;
-        EXPECT_EQ(msg.data, uint64_received->data);
+            count++;
+        }
+        ros::shutdown();
+        printf("-111111111111111111111111111hahahha\n");
+
+    }
+    else
+    {
+        printf("father2 %d\n", getpid());
+        int argc = 1;
+        char * argv[] = {(char *)"shm_listener", NULL};
+        ros::init(argc, (char **)argv, "shm_listener", ros::init_options::AnonymousName);
+        ros::NodeHandle n;
+        shm_transport::Topic t(n);
+        shm_transport::Subscriber< std_msgs::UInt64 > s = t.subscribe("/shm_test_topic1", 60, uint64Callback);
+        ros::Rate loop_rate(60);
+        while(ros::ok && count <= 5)
+        {
+            //ROS_INFO("%d",count);
+            loop_rate.sleep();
+            ros::spinOnce();
+        }
+        ros::shutdown();
+
+        pid_t wait = waitpid(child, NULL, WNOHANG);
+        while(wait == 0)
+        {
+            printf("i am waitiong for child process\n");
+            sleep(1);
+            wait = waitpid(child, NULL, WNOHANG);
+        }
+        if(wait == child)
+        {
+            printf("i am father process, exiting\n");
+        }
+        printf("-**************************hahahha\n");
     }
 }
 
 int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
-    ros::init(argc, argv, "shm_talker", ros::init_options::AnonymousName);
+    //ros::init(argc, argv, "shm_test", ros::init_options::AnonymousName);
 
     return RUN_ALL_TESTS();
 }
